d46c813bf6d5f5660e7571d022c4a9decc803d32
unknown <priyankaxworkz179@gmail.com>
2021-09-02 15:57:16 +0530

    added demo file

---
+++ CountryName.java
@@ -0,0 +1,28 @@
+class CountryName{
+public static void main(String a[])
+{
+String[] countryName= new String[7];
+countryName[0]="India";
+countryName[1]="Sri Lanka";
+countryName[2]="England";
+countryName[3]="China";
+countryName[4]="USA";
+countryName[5]="Pakistan";
+countryName[6]="Andorra";
+
+for(int z=0;z < countryName.length;z++)
+{
+System.out.println(countryName[z]);
+
+
+
+}
+
+
+
+}
+
+
+
+
+}
