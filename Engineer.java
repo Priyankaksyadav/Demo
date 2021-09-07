9700fc4cc7782d5867db83b7ee4e39964cd92a31
unknown <priyankaxworkz179@gmail.com>
2021-09-07 14:56:07 +0530

    added

---
+++ Engineer.java
@@ -0,0 +1,10 @@
+class Engineer{
+public static void main(String a[]){
+String[]  branchName={"EC","TCE","CS","IS","CV"};
+int[]    noOfInTake={100,50,80,80,50};
+
+System.out.println( branchName[0]+" "+ branchName[1]+" "+ branchName[2]+" "+ branchName[3]+" "+ branchName[4]);
+System.out.println(noOfInTake[0]+" "+noOfInTake[1]+" "+noOfInTake[2]+" "+noOfInTake[3]+" "+noOfInTake[4]);
+
+}
+}
