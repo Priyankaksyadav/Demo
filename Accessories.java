b779638d0abeafff548c2f2b35478edf8eec5e0e
unknown <priyankaxworkz179@gmail.com>
2021-09-07 14:27:20 +0530

    added java files

---
+++ Accessories.java
@@ -0,0 +1,13 @@
+class Accesories{
+public static void main(String a[]){
+String  Name="Necklace";
+int     cost=50000;
+int     gram=10;
+boolean   isGold=true;
+System.out.println(Name);
+System.out.println(cost);
+System.out.println(gram);
+System.out.println(isGold);
+
+}
+}
---
+++ AddNumber.java
@@ -0,0 +1,15 @@
+class AddTwoNumbers{
+public static void main(String a[])
+{
+add(18,29);
+add(55,85);
+add(90,26);
+}
+public static void add(int x, int y){
+System.out.println(x+y);
+}
+
+  
+
+
+}
